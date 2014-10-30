cd84143dbccb96b6e401bf97e3d5d4269ceeebd4
Ted Han <ted@knowtheory.net>
2014-10-30 16:04:49 -0500

    Sorted out borrowing.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 extern crate getopts;
+extern crate time;
+extern crate postgres;
+
 use getopts::{optopt,optflag,getopts,OptGroup};
+
 use std::os;
 use std::io::fs::PathExtensions;
 use std::io::BufferedReader;
 use std::io::File;
 
+use time::{Timespec, Tm};
+
+use postgres::{PostgresConnection, NoSsl};
+
 struct Entry {
       bucket_owner:     String,
       bucket:           String,
-      time:             String,
+      time:             Tm,
       remote_ip:        String,
       requester:        String,
       request_id:       String,
@@ -32,7 +40,7 @@ fn entry_for(values: Vec<String>) -> Entry {
     Entry {
         bucket_owner:     values[0].to_string(),
         bucket:           values[1].to_string(),
-        time:             values[2].to_string(),
+        time:             time::strptime(values[2].as_slice(), "%d/%b/%Y:%H:%M:%S %z").unwrap(),
         remote_ip:        values[3].to_string(),
         requester:        values[4].to_string(),
         request_id:       values[5].to_string(),
@@ -51,10 +59,70 @@ fn entry_for(values: Vec<String>) -> Entry {
     }
 }
 
-fn process(entry: &Entry) -> uint {
-    let value = entry.bytes_sent;
-    //println!("Value is: {}", value);
-    value
+fn process(entry: &Entry) -> String {
+    let mut sql = String::new();
+    sql.push('(');
+    //println!("Value is: {}", entry.time);
+
+    sql.push_str(entry.bucket_owner.as_slice());
+    sql.push_str(entry.bucket.as_slice());
+    sql.push_str(entry.time.to_string().as_slice());
+    sql.push_str(entry.remote_ip.as_slice());
+    sql.push_str(entry.requester.as_slice());
+    sql.push_str(entry.request_id.as_slice());
+    sql.push_str(entry.operation.as_slice());
+    sql.push_str(entry.aws_key.as_slice());
+    sql.push_str(entry.request_uri.as_slice());
+    sql.push_str(entry.status_code.as_slice());
+    sql.push_str(entry.error_code.as_slice());
+    sql.push_str(entry.bytes_sent.to_string().as_slice());
+    sql.push_str(entry.object_size.to_string().as_slice());
+    sql.push_str(entry.total_time.to_string().as_slice());
+    sql.push_str(entry.turn_around_time.to_string().as_slice());
+    sql.push_str(entry.referrer.as_slice());
+    sql.push_str(entry.user_agent.as_slice());
+    sql.push_str(entry.version_id.as_slice());
+    
+    sql.push(')');
+    sql
+}
+
+fn insert_entry(conn: &PostgresConnection, entry: &Entry) {
+    let statement = conn.prepare("insert into entries (bucket_owner, bucket, time, remote_ip, requester, request_id, operation, aws_key, request_uri, status_code, error_code, bytes_sent, object_size, total_time, turn_around_time, referrer, user_agent, version_id) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)").unwrap();
+    
+    statement.execute(
+        &[
+            &entry.bucket_owner,
+            &entry.bucket,
+            &entry.time.to_timespec(),
+            &entry.remote_ip,
+            &entry.requester,
+            &entry.request_id,
+            &entry.operation,
+            &entry.aws_key,
+            &entry.request_uri,
+            &entry.status_code,
+            &entry.error_code,
+            &entry.bytes_sent.to_string(),
+            &entry.object_size.to_string(),
+            &entry.total_time.to_string(),
+            &entry.turn_around_time.to_string(),
+            &entry.referrer,
+            &entry.user_agent,
+            &entry.version_id,
+        ]
+    );
+}
+
+fn count_rows(conn: &PostgresConnection) {
+    let statement = conn.prepare("select count(*) from entries;").unwrap();
+    for res in statement.query([]).unwrap() {
+        let result : Option<i64> = res.get(0);
+        match result {
+            Some(x) => println!("Derp? {}", x),
+            None => println!("What went wrong?! {}", result),
+        }
+    }
 }
 
 fn main() {
@@ -65,8 +133,9 @@ fn main() {
             println!("Log file exists at \"{}\"", path.display());
             let mut file = BufferedReader::new(File::open(&path));
 
+            let conn = PostgresConnection::connect("postgresql://ted@localhost:5432/dcloud_s3_analytics",&NoSsl).unwrap();
+
             let mut count = 0u;
-            let mut total = 0u;
             let mut entries = Vec::new();
             let mut tokens = Vec::new();
             let mut token = String::new();
@@ -87,9 +156,10 @@ fn main() {
                     //println!("{}", tokens);
                     tokens.truncate(0);
                     if entries.len() >= 1000 {
-                        for entry in entries.iter() { total += process(entry); }
+                        for entry in entries.iter() { insert_entry(&conn, entry); }
                         entries.truncate(0);
                         count += 1000;
+                        count_rows(&conn);
                         println!("Running Count: {}", count);
                         //println!("Running total: {}", total);
                     }
@@ -114,8 +184,8 @@ fn main() {
                     token.push(c);
                 }
             }
-            for entry in entries.iter() { total += process(entry); }
-            println!("Total: {}", total);
+            for entry in entries.iter() { insert_entry(&conn, entry); }
+            count_rows(&conn);
         } else {
             println!("Log file doesn't exist at \"{}\"", path.display());
         }
