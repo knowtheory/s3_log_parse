19d04e5e34c360a62baa684ef37da986a0773950
Ted Han <ted@knowtheory.net>
2015-07-30 16:05:03 -0700

    No recollection of what's going on here… so just committing before rework.

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ use std::io::fs::PathExtensions;
 use std::io::BufferedReader;
 use std::io::File;
 
+use std::fmt;
+
 use time::{Timespec, Tm};
 
 use postgres::{PostgresConnection, NoSsl};
+use postgres::types::ToSql;
 
 struct Entry {
       bucket_owner:     String,
@@ -25,93 +28,75 @@ struct Entry {
       request_uri:      String,
       status_code:      String,
       error_code:       String,
-      bytes_sent:       uint,
-      object_size:      uint,
-      total_time:       uint,
-      turn_around_time: uint,
+      bytes_sent:       i32,
+      object_size:      i32,
+      total_time:       i32,
+      turn_around_time: i32,
       referrer:         String,
       user_agent:       String,
       version_id:       String,
 }
 
-fn entry_for(values: Vec<String>) -> Entry {
-    if values.len() <= 17 { println!("Incorrect number of fields in: {}", values); }
-    //println!("{}", values.len());
-    Entry {
-        bucket_owner:     values[0].to_string(),
-        bucket:           values[1].to_string(),
-        time:             time::strptime(values[2].as_slice(), "%d/%b/%Y:%H:%M:%S %z").unwrap(),
-        remote_ip:        values[3].to_string(),
-        requester:        values[4].to_string(),
-        request_id:       values[5].to_string(),
-        operation:        values[6].to_string(),
-        aws_key:          values[7].to_string(),
-        request_uri:      values[8].to_string(),
-        status_code:      values[9].to_string(),
-        error_code:       values[10].to_string(),
-        bytes_sent:       from_str::<uint>(values[11].as_slice()).unwrap_or(0u),
-        object_size:      from_str::<uint>(values[12].as_slice()).unwrap_or(0u),
-        total_time:       from_str::<uint>(values[13].as_slice()).unwrap_or(0u),
-        turn_around_time: from_str::<uint>(values[14].as_slice()).unwrap_or(0u),
-        referrer:         values[15].to_string(),
-        user_agent:       values[16].to_string(),
-        version_id:       values[17].to_string(),
+impl Entry {
+    fn new(values: Vec<String>) -> Entry {
+        if values.len() <= 17 { println!("Incorrect number of fields in: {}", values); }
+        //println!("{}", values.len());
+        Entry {
+            bucket_owner:     values[0].to_string(),
+            bucket:           values[1].to_string(),
+            time:             time::strptime(values[2].as_slice(), "%d/%b/%Y:%H:%M:%S %z").unwrap(),
+            remote_ip:        values[3].to_string(),
+            requester:        values[4].to_string(),
+            request_id:       values[5].to_string(),
+            operation:        values[6].to_string(),
+            aws_key:          values[7].to_string(),
+            request_uri:      values[8].to_string(),
+            status_code:      values[9].to_string(),
+            error_code:       values[10].to_string(),
+            bytes_sent:       from_str(values[11].as_slice()).unwrap_or(0),
+            object_size:      from_str(values[12].as_slice()).unwrap_or(0),
+            total_time:       from_str(values[13].as_slice()).unwrap_or(0),
+            turn_around_time: from_str(values[14].as_slice()).unwrap_or(0),
+            referrer:         values[15].to_string(),
+            user_agent:       values[16].to_string(),
+            version_id:       values[17].to_string(),
+        }
     }
 }
 
-fn process(entry: &Entry) -> String {
-    let mut sql = String::new();
-    sql.push('(');
-    //println!("Value is: {}", entry.time);
-
-    sql.push_str(entry.bucket_owner.as_slice());
-    sql.push_str(entry.bucket.as_slice());
-    sql.push_str(entry.time.to_string().as_slice());
-    sql.push_str(entry.remote_ip.as_slice());
-    sql.push_str(entry.requester.as_slice());
-    sql.push_str(entry.request_id.as_slice());
-    sql.push_str(entry.operation.as_slice());
-    sql.push_str(entry.aws_key.as_slice());
-    sql.push_str(entry.request_uri.as_slice());
-    sql.push_str(entry.status_code.as_slice());
-    sql.push_str(entry.error_code.as_slice());
-    sql.push_str(entry.bytes_sent.to_string().as_slice());
-    sql.push_str(entry.object_size.to_string().as_slice());
-    sql.push_str(entry.total_time.to_string().as_slice());
-    sql.push_str(entry.turn_around_time.to_string().as_slice());
-    sql.push_str(entry.referrer.as_slice());
-    sql.push_str(entry.user_agent.as_slice());
-    sql.push_str(entry.version_id.as_slice());
-    
-    sql.push(')');
-    sql
+impl fmt::Show for Entry {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "ENTRY[\"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\"]", self.bucket_owner, self.bucket, self.time, self.remote_ip, self.requester, self.request_id, self.operation, self.aws_key, self.request_uri, self.status_code, self.error_code, self.bytes_sent, self.object_size, self.total_time, self.turn_around_time, self.referrer, self.user_agent, self.version_id )
+    }
 }
 
 fn insert_entry(conn: &PostgresConnection, entry: &Entry) {
-    let statement = conn.prepare("insert into entries (bucket_owner, bucket, time, remote_ip, requester, request_id, operation, aws_key, request_uri, status_code, error_code, bytes_sent, object_size, total_time, turn_around_time, referrer, user_agent, version_id) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)").unwrap();
+    let statement = conn.prepare("insert into entries (bucket_owner, bucket, time, remote_ip, requester, request_id, operation, aws_key, request_uri, status_code, error_code, bytes_sent, object_size, total_time, turn_around_time, referrer, user_agent, version_id) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)").unwrap();
     
-    statement.execute(
-        &[
-            &entry.bucket_owner,
-            &entry.bucket,
-            &entry.time.to_timespec(),
-            &entry.remote_ip,
-            &entry.requester,
-            &entry.request_id,
-            &entry.operation,
-            &entry.aws_key,
-            &entry.request_uri,
-            &entry.status_code,
-            &entry.error_code,
-            &entry.bytes_sent.to_string(),
-            &entry.object_size.to_string(),
-            &entry.total_time.to_string(),
-            &entry.turn_around_time.to_string(),
-            &entry.referrer,
-            &entry.user_agent,
-            &entry.version_id,
-        ]
-    );
+    let result = statement.execute(&[
+        &entry.bucket_owner,
+        &entry.bucket,
+        &entry.time.to_timespec(),
+        &entry.remote_ip,
+        &entry.requester,
+        &entry.request_id,
+        &entry.operation,
+        &entry.aws_key,
+        &entry.request_uri,
+        &entry.status_code,
+        &entry.error_code,
+        &entry.bytes_sent,
+        &entry.object_size,
+        &entry.total_time,
+        &entry.turn_around_time,
+        &entry.referrer,
+        &entry.user_agent,
+        &entry.version_id,
+    ]);
+    match result {
+        Ok(x) => (),
+        Err(x) => println!("{} for entry {}", x, &entry),
+    };
 }
 
 fn count_rows(conn: &PostgresConnection) {
@@ -144,26 +129,26 @@ fn main() {
             for c_wrap in file.chars() {
                 let c = c_wrap.unwrap();
 
+                // Newline always terminates the line & resets the terminator.
                 if c == '\n' {
                     //println!("Final line token: \"{}\", and terminator is: '{}'", token, terminator);
                     tokens.push(token.clone());
                     if !(tokens.len() < 17) { 
-                        entries.push(entry_for(tokens.clone())); 
+                        entries.push(Entry::new(tokens.clone())); 
                     } else {
                         println!("Parsing error for: {}", tokens);
                     }
                     token.truncate(0);
                     //println!("{}", tokens);
                     tokens.truncate(0);
-                    if entries.len() >= 1000 {
+                    if entries.len() >= 10000 {
                         for entry in entries.iter() { insert_entry(&conn, entry); }
                         entries.truncate(0);
-                        count += 1000;
-                        count_rows(&conn);
-                        println!("Running Count: {}", count);
+                        count += 10000;
+                        //count_rows(&conn);
                         //println!("Running total: {}", total);
                     }
-                    terminator = ' ' // guarantee that a newline terminates the line.
+                    terminator = ' '; // guarantee that a newline terminates the line.
                 } else if skip_next {
                     skip_next = false;
                 } else if c == terminator {
@@ -185,7 +170,7 @@ fn main() {
                 }
             }
             for entry in entries.iter() { insert_entry(&conn, entry); }
-            count_rows(&conn);
+            //count_rows(&conn);
         } else {
             println!("Log file doesn't exist at \"{}\"", path.display());
         }
